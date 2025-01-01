51ff73dc0980340766b7bb9cde96c7a0ddc79b78
abhishekSinghNitian <abhishekfbg940@gmail.com>
2025-01-01 10:40:43 +0530

    one more commit

--- config/typeorm.config.ts
+++ config/typeorm.config.ts
@@ -1,9 +1,4 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { College } from "src/colleges/entities/college.entity";
-import { Placement } from "src/placements/entities/placement.entity";
-import { State } from "src/states/entities/state.entity";
-import { City } from "src/cities/entities/city.entity";
-import { Course } from "src/courses/entities/course.entity";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -12,6 +7,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: [College, Placement, State, City, Course],
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,18 @@
 import { DataSource } from 'typeorm';
+import { User } from './users/entities/user.entity';
 import { State } from './states/entities/state.entity';
 import { City } from './cities/entities/city.entity';
 import { College } from './colleges/entities/college.entity';
 import { Course } from './courses/entities/course.entity';
 import { Placement } from './placements/entities/placement.entity';
-import { User } from './users/entities/user.entity';
 
 export const AppDataSource = new DataSource({
-  type: 'postgres', // or your database type
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'Abhi@07102003',
-  database: 'sports_duniya',
-  entities: [State, City, College, Course, Placement, User],
-  synchronize: true,
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [User, State, City, College, Course, Placement],
+  synchronize: false,
 });
